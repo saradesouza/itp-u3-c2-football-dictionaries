7816ecaf24f1430efbba5dc0aa754f0577dff005
Sara de Souza <saradesouza87@gmail.com>
2017-12-01 14:33:10 +0000

    first commit assigmment 1

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,18 @@
-def players_as_dictionaries(squads_list):
-    pass
+from squads_data import SQUADS_DATA
+
+squad_keys = ['number','position','name','date_of_birth','caps','club','country','club_country','year']
+
+footballerDict = {}
+
+for i in range(0, len(SQUADS_DATA)):
+    for j in range(0, len(SQUADS_DATA[i])):
+        print(squad_keys[j],SQUADS_DATA[i][j])
+        footballerDict[squad_keys[j]] = SQUADS_DATA[i][j]
+        # footballerDict.update({squad_keys[j]:SQUADS_DATA[i][j]})
+
+# footballerOne.update({squad_keys[0]:SQUADS_DATA[0][0]})
+# footballerOne.update({squad_keys[1]:SQUADS_DATA[0][1]})
+
+playerList = [footballerDict]
+
+print(playerList)
